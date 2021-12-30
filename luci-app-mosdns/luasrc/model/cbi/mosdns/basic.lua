7c45f97a6db9d4fb055384fc5ac8f9be9755301a
Jan20th9e <55278556+Jan20th9e@users.noreply.github.com>
2021-12-31 02:09:47 +0800

    Update basic.lua

--- luci-app-mosdns/luasrc/model/cbi/mosdns/basic.lua
+++ luci-app-mosdns/luasrc/model/cbi/mosdns/basic.lua
@@ -14,20 +14,4 @@ enable.rmempty = false
 redirect = s:option(Flag, "redirect", translate("Enable DNS Redirect"))
 redirect.rmempty = false
 
-set_config = s:option(Button, "set_config", translate("DNS Helper"))
-set_config.inputtitle = translate("Apply")
-set_config.inputstyle = "reload"
-set_config.description = translate("This will make the necessary adjustments to other plug-in settings.")
-set_config.write = function()
-  luci.sys.exec("/etc/mosdns/set.sh >/dev/null 2>&1 &")
-end
-
-unset_config = s:option(Button, "unset_config", translate("Revert Settings"))
-unset_config.inputtitle = translate("Apply")
-unset_config.inputstyle = "reload"
-unset_config.description = translate("This will revert the adjustments.")
-unset_config.write = function()
-  luci.sys.exec("/etc/mosdns/unset.sh >/dev/null 2>&1 &")
-end
-
 return m
