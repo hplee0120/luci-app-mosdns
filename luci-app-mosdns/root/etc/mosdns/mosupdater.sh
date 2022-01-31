2401e3c40e99334a12e4c7bb9e299f7222940c57
Jan20th9e <55278556+Jan20th9e@users.noreply.github.com>
2022-01-31 09:55:29 +0800

    Update mosupdater.sh

--- luci-app-mosdns/root/etc/mosdns/mosupdater.sh
+++ luci-app-mosdns/root/etc/mosdns/mosupdater.sh
@@ -2,8 +2,9 @@
 set -o pipefail
 rm -rf  /tmp/mosdns
 mkdir /tmp/mosdns
-wget https://cdn.jsdelivr.net/gh/Loyalsoldier/geoip@release/cn.dat -nv -O /tmp/mosdns/cn.dat
-wget https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -nv -O /tmp/mosdns/geosite.dat
+wget https://cdn.jsdelivr.net/gh/Loyalsoldier/domain-list-custom@release/geosite.dat -nv -O /tmp/mosdns/geosite.dat
+wget https://ispip.clang.cn/all_cn.txt -nv -O /tmp/mosdns/all_cn.txt
+wget https://ispip.clang.cn/all_cn_ipv6.txt -nv -O /tmp/mosdns/all_cn_ipv6.txt
 find /tmp/mosdns/* -size -20k -exec rm {} \;
 chmod -R  755  /tmp/mosdns
 cp -rf /tmp/mosdns/* /etc/mosdns
