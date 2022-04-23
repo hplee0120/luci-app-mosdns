e7071a9e27c5fad10e0711b0b8d3987c461a7a0d
Jan20th9e <55278556+Jan20th9e@users.noreply.github.com>
2022-04-24 04:40:11 +0800

    Update mosupdater.sh

--- luci-app-mosdns/root/etc/mosdns/mosupdater.sh
+++ luci-app-mosdns/root/etc/mosdns/mosupdater.sh
@@ -2,8 +2,9 @@
 set -o pipefail
 rm -rf  /tmp/mosdns
 mkdir /tmp/mosdns
-wget https://mirrors.tuna.tsinghua.edu.cn/osdn/storage/g/v/v2/v2raya/dists/v2ray-rules-dat/geosite.dat -nv -O /tmp/mosdns/geosite.dat
-wget https://mirrors.tuna.tsinghua.edu.cn/osdn/storage/g/v/v2/v2raya/dists/geoip/cn.dat -nv -O /tmp/mosdns/geoip.dat
+wget https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/chnlist.txt -nv -O /tmp/mosdns/chnlist.txt
+wget https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/gfwlist.txt -nv -O /tmp/mosdns/gfwlist.txt
+wget https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/geoip.dat -nv -O /tmp/mosdns/geoip.dat
 find /tmp/mosdns/* -size -20k -exec rm {} \;
 chmod -R  755  /tmp/mosdns
 cp -rf /tmp/mosdns/* /etc/mosdns
