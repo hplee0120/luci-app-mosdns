24c6201317b50eba444a076218fe90af6cede456
Jan20th9e <55278556+Jan20th9e@users.noreply.github.com>
2022-04-25 21:09:58 +0800

    Update mosupdater.sh

--- luci-app-mosdns/root/etc/mosdns/mosupdater.sh
+++ luci-app-mosdns/root/etc/mosdns/mosupdater.sh
@@ -2,9 +2,9 @@
 set -o pipefail
 rm -rf  /tmp/mosdns
 mkdir /tmp/mosdns
-wget https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/chnlist.txt -nv -O /tmp/mosdns/chnlist.txt
-wget https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/gfwlist.txt -nv -O /tmp/mosdns/gfwlist.txt
-wget https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/geoip.dat -nv -O /tmp/mosdns/geoip.dat
+wget https://ghproxy.com/https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/chnlist.txt -nv -O /tmp/mosdns/chnlist.txt
+wget https://ghproxy.com/https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/gfwlist.txt -nv -O /tmp/mosdns/gfwlist.txt
+wget https://ghproxy.com/https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/geoip.dat -nv -O /tmp/mosdns/geoip.dat
 find /tmp/mosdns/* -size -20k -exec rm {} \;
 chmod -R  755  /tmp/mosdns
 cp -rf /tmp/mosdns/* /etc/mosdns
