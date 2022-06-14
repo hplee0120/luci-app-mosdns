7e361824228a748ff8e30afe59bd2133a6baeb15
hplee0120 <55278556+hplee0120@users.noreply.github.com>
2022-06-14 09:53:49 +0800

    Update mosupdater.sh

--- luci-app-mosdns/root/etc/mosdns/mosupdater.sh
+++ luci-app-mosdns/root/etc/mosdns/mosupdater.sh
@@ -2,9 +2,9 @@
 set -o pipefail
 rm -rf  /tmp/mosdns
 mkdir /tmp/mosdns
-wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/chinalist.txt -nv -O /tmp/mosdns/chinalist.txt
-wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/gfwlist.txt -nv -O /tmp/mosdns/gfwlist.txt
-wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/Jan20th9e/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/geoip.dat -nv -O /tmp/mosdns/geoip.dat
+wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/hplee0120/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/chinalist.txt -nv -O /tmp/mosdns/chinalist.txt
+wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/hplee0120/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/gfwlist.txt -nv -O /tmp/mosdns/gfwlist.txt
+wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/hplee0120/openwrt-mos/master/luci-app-mosdns/root/etc/mosdns/geoip.dat -nv -O /tmp/mosdns/geoip.dat
 find /tmp/mosdns/* -size -20k -exec rm {} \;
 chmod -R  755  /tmp/mosdns
 cp -rf /tmp/mosdns/* /etc/mosdns
