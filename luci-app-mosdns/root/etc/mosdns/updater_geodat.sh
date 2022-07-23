e97c6629166d20b8f5fe002ffd890d4496b4fdba
hplee0120 <55278556+hplee0120@users.noreply.github.com>
2022-07-24 07:57:34 +0800

    Update updater_geodat.sh

--- luci-app-mosdns/root/etc/mosdns/updater_geodat.sh
+++ luci-app-mosdns/root/etc/mosdns/updater_geodat.sh
@@ -2,9 +2,8 @@
 set -o pipefail
 rm -rf  /tmp/mosdns
 mkdir /tmp/mosdns
-wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/hplee0120/luci-app-mosdns/master/luci-app-mosdns/root/etc/mosdns/chinalist.txt -nv -O /tmp/mosdns/chinalist.txt
-wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/hplee0120/luci-app-mosdns/master/luci-app-mosdns/root/etc/mosdns/gfwlist.txt -nv -O /tmp/mosdns/gfwlist.txt
-wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/hplee0120/luci-app-mosdns/master/luci-app-mosdns/root/etc/mosdns/geoip.dat -nv -O /tmp/mosdns/geoip.dat
+wget https://cdn.divineengine.net/gh/https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -nv -O /tmp/mosdns/geosite.dat
+wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/Loyalsoldier/geoip/release/cn.dat -nv -O /tmp/mosdns/geoip.dat
 find /tmp/mosdns/* -size -20k -exec rm {} \;
 chmod -R  755  /tmp/mosdns
 cp -rf /tmp/mosdns/* /etc/mosdns
