7151b898ca1a6852d56bcf5595ab8328e6dd1e41
hplee0120 <55278556+hplee0120@users.noreply.github.com>
2022-08-16 14:51:53 +0800

    Update updater_geodat.sh

--- luci-app-mosdns/root/etc/mosdns/updater_geodat.sh
+++ luci-app-mosdns/root/etc/mosdns/updater_geodat.sh
@@ -3,6 +3,7 @@ set -o pipefail
 rm -rf  /tmp/mosdns
 mkdir /tmp/mosdns
 wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/hplee0120/luci-app-mosdns/master/luci-app-mosdns/root/etc/mosdns/chinalist.txt -nv -O /tmp/mosdns/chinalist.txt
+wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/hplee0120/luci-app-mosdns/master/luci-app-mosdns/root/etc/mosdns/gfwlist.txt -nv -O /tmp/mosdns/gfwlist.txt
 wget https://cdn.divineengine.net/gh/https://raw.githubusercontent.com/hplee0120/luci-app-mosdns/master/luci-app-mosdns/root/etc/mosdns/geoip.dat -nv -O /tmp/mosdns/geoip.dat
 find /tmp/mosdns/* -size -20k -exec rm {} \;
 chmod -R  755  /tmp/mosdns
